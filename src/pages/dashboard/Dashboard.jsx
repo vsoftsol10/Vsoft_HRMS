0532fd5af694d3ce01a4204fa8d552fe3c108d26
Nirmal <nirmalsep2001@gmail.com>
2025-06-02 10:17:17 +0530

    Navbar and Login page created

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar";
+
+const Dashboard=()=>{
+  return ( <>
+  
+  
+  <Navbar/>
+  <div className="dashBoardContainer">
+      <h1 className="header" style={{textAlign:"center"}}>DashBoard</h1>
+  </div>
+
+  </>
+)}
+export default Dashboard;
